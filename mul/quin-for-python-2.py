6063b56ebf43206c9670cdcb59311052dbc73049
Elisha Hollander <just4now666666@gmail.com>
2020-12-03 19:33:24 +0200

    Create quin-for-python-2.py

---
+++ mul/quin-for-python-2.py
@@ -0,0 +1,10 @@
+#include/*
+q='''*/<stdio.h>
+main(){char* _;/*=;sub _:lvalue{$_}<<q;#';<<q#'''
+def printf(a, *b):print a%b,
+q
+#*/
+_="#include/*%cp='''*/<stdio.h>%cmain(){char * _;/*=;sub _:lvalue{%c_}<<q;#';<<q#'''%cdef printf(a, *b):print a%%b,%cq%c#*/%c_=%c%s%c;printf(_,10,10,36,10,10,10,10,34,_,34,10,10,10,10);%c#/*%cp='''*/%c}//'''#=%c";printf(_,10,10,36,10,10,10,10,34,_,34,10,10,10,10);
+#/*
+q='''*/
+}//'''#=
