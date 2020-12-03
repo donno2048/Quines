d525a0204bf66114167a9a4b29b6a191b422362c
Elisha Hollander <just4now666666@gmail.com>
2020-12-04 01:22:25 +0200

    Create quin.cc

---
+++ quin.cc
@@ -0,0 +1,3 @@
+#include<iostream>
+#include<string>
+auto main(void)->int{using namespace std;[](string s1,string s2,string s3,string s4){cout<<s1<<endl<<s2<<endl<<s3<<(char)34<<s1<<(char)34<<(char)44<<(char)34<<s2<<(char)34<<(char)44<<(char)34<<s3<<(char)34<<(char)44<<(char)34<<s4<<(char)34<<s4<<endl;}("#include<iostream>","#include<string>","auto main(void)->int{using namespace std;[](string s1,string s2,string s3,string s4){cout<<s1<<endl<<s2<<endl<<s3<<(char)34<<s1<<(char)34<<(char)44<<(char)34<<s2<<(char)34<<(char)44<<(char)34<<s3<<(char)34<<(char)44<<(char)34<<s4<<(char)34<<s4<<endl;}(",");}");}
