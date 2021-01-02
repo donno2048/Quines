7ad13b85c27a11e61c11497c5891ea8fcf307583
Elisha Hollander <just4now666666@gmail.com>
2021-01-03 01:33:08 +0200

    Create quin.rs

---
+++ bin/quin.rs
@@ -0,0 +1,1 @@
+fn f(o:u8){for c in b"go!g)n;t9(zgns!b!ho!c#%#/huds)(zhg!b_n <27zqshou )#z;19c|#-b_n(:|dmrdzg)1(:|||go!l`ho)(zg)0(:|".iter(){if c^o!=36{print!("{:08b}",c^o);}else{f(0);}}}fn main(){f(1);}
