153001a36d67d525a10a4a06dc7faa8fe104f1bf
Elisha Hollander <just4now666666@gmail.com>
2021-03-08 01:36:17 +0200

    Create quin.rb

---
+++ rough/quin.rb
@@ -0,0 +1,1 @@
+eval s=%w(0;y=""<<32;z="eval((;s=%w(#{s} )*''));; %"<<43<<s;d="";" 0v vvnvn2 mj4kil 0v lvvul2 6l94ol 0v lv7vv".scan(/.{7}/){|n|2.times{d.<<Time.now.to_s.unpack(" C*")[11,8].map{|i|("%03b".%7&n.to_i(32)>>3*i-=48).gsub(/./){s>$&?y*3:z.slice!(0,3)}}*y<<10}};puts(d.strip.chop<<43))*''
